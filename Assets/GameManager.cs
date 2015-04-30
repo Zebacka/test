dd2d808c44be1677f1b6b4213ef2bfd98fcc172e
Zeb Mackenzie <zeb0396@gmail.com>
2015-04-30 15:40:37 +1000

    1.02
    Small changes

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public float StartTime;
+	public string CurrentTime;
+
+	public Rect timerRect;
+
+	public GUISkin skin;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartTime += Time.deltaTime;
+		CurrentTime = string.Format ("{0:0.0}", StartTime);
+
+
+	}
+
+
+	void OnGUI()
+	{
+		GUI.Label (timerRect, CurrentTime);
+
+	}
+
+
+
+
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+	public float StartTime;
+	public string CurrentTime;
+	
+	public Rect timerRect;
+	
+	public Text text123;
+	public Object object1;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		StartTime += Time.deltaTime;
+		CurrentTime = string.Format ("{0:0.00}", StartTime);
+
+		text123 = GetComponent<Text>();
+		text123.text = CurrentTime;
+	}
+}
