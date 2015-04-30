94e3e7e3f4105cf0f9759a363561789cee008f84
Zeb Mackenzie <zeb0396@gmail.com>
2015-04-30 12:26:27 +1000

    1.0
    Got assets working intended + second level

---
+++ Assets/next.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class next : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("FIRST");
+		if (other.gameObject.tag == "Player")
+			Debug.Log("Hit");
+			Application.LoadLevel("2");
+	}
+}
